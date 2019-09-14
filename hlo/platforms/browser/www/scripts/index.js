e09a75e6bde339c82b2c58a5f206489129453915
AyushMaanJain <ayushjain1133@gmail.com>
2019-09-14 10:36:49 +0530

    calculator created in cordova by jquery
    find any bug if you can
    
    Note: this calculator has not input box it is created only for touch device

---
+++ hlo/platforms/browser/www/scripts/index.js
@@ -0,0 +1,39 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    if ($.isWin) {
+        document.getElementById('themeStylesheet').setAttribute('href', './css/chui-win-3.8.5.css');
+    } else if ($.isAndroid) {
+        document.getElementById('themeStylesheet').setAttribute('href', './css/chui-android-3.8.5.css');
+    } else if ($.isiOS) {
+        document.getElementById('themeStylesheet').setAttribute('href', './css/chui-ios-3.8.5.css');
+    } else {
+        document.getElementById('themeStylesheet').setAttribute('href', './css/chui-win-3.8.5.css');
+    }
+    var tabbarOP = {
+        tabs: 4,
+        icons: ["home", "drinks", "cakes", "pizzas"],
+        labels: ["Home", "Drinks", "Cakes", "Pizzas"],
+        selected: 1
+    };
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+        $.UITabbar(tabbarOP);
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    };
+
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
